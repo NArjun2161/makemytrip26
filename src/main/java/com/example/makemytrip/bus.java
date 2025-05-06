847831de13e9e4b171b1d08775cf1d1c096b8468
Arjun Narale <160503114+NArjun2161@users.noreply.github.com>
2025-05-06 21:42:46 +0530

    Update bus.java

--- src/main/java/com/example/makemytrip/bus.java
+++ src/main/java/com/example/makemytrip/bus.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class bus {
     @GetMapping("/bus")
-    public String getData() {return  "as per client testing requirement is fullfill BUS Microservice" ; }
+    public String getData() {return  "as per client testing requirement fullfill BUS Microservice" ; }
 }
