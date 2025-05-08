c2a400e55363dd8bbc0469da37ec6af3eaf08b51
Arjun Narale <160503114+NArjun2161@users.noreply.github.com>
2025-05-08 15:29:49 +0530

    Update flights.java

--- src/main/java/com/example/makemytrip/flights.java
+++ src/main/java/com/example/makemytrip/flights.java
@@ -4,10 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FlightsController {
-
+public class flights {
     @GetMapping("/flights")
-    public String getData() {
-        return "Please book your hotels from MMT. Kindly book a ticket from New Delhi to anywhere at 20% discount.";
-    }
+    public String getData() {return  "Please book your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
 }
